b4d0594640b45b17f243604a046fff7c470d7802
thacuber2a03 <70547062+thacuber2a03@users.noreply.github.com>
2024-07-16 20:30:09 -0400

    build.bat has lib instead of winlib

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 
-set locs=-Llib -Iinclude
+set locs=-Lwinlib -Iinclude
 set libs=-lwren -lraylib -lgdi32 -lopengl32 -lwinmm
 
 gcc src\*.c -o txt %locs% %libs% -Wno-discarded-qualifiers -static
